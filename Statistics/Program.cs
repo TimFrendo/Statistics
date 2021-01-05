60bcda4aea6610a8d754b0a7641d0e634cf5f528
TimFrendo <tirnagog123@gmail.com>
2021-01-05 17:04:11 +0600

    Добавьте файлы проекта.

---
+++ Statistics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[10] { 0.111, 0.1, 0.5, 1.0, 0.2, 0.3, 5, 7, 0, 1};
+            var fd = Statistics.Mode(arr);
+            Console.WriteLine(fd);
+        }
+        
+    }
+}
---
+++ Statistics/Statistics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    static class Statistics
+    {
+        public static double Mode(double[] arr)
+        {
+            Dictionary<double, int> frequency = new Dictionary<double, int>();
+            double mode = 0.0;
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!frequency.ContainsKey(arr[i]))
+                    frequency.Add(arr[i], 1);
+                else
+                    frequency[arr[i]] += 1;
+            }
+            foreach (var freq in frequency)
+            {
+                if (temp < freq.Value)
+                {
+                    temp = freq.Value;
+                    mode = freq.Key;
+                }
+            }
+            return mode;
+        }
+    }
+}
