d5d9c9525faf6782e288fe9d6d8a2990f474ba51
TimFrendo <tirnagog123@gmail.com>
2021-01-05 18:21:31 +0600

    add methods Mediand, Mean, Maximum, Minimum, Span

--- Statistics/Program.cs
+++ Statistics/Program.cs
@@ -7,9 +7,20 @@ namespace Statistics
     {
         static void Main(string[] args)
         {
-            double[] arr = new double[10] { 0.111, 0.1, 0.5, 1.0, 0.2, 0.3, 5, 7, 0, 1};
-            var fd = Statistics.Mode(arr);
-            Console.WriteLine(fd);
+            double[] arr = new double[13] {-2,  -1, 0, 1, -2, 3.2, 1.9, 1.9, 6.1, 1.9, 3.8, 7.9, 0 };
+            Console.WriteLine($"length of array: {arr.Length}");
+            var mode = Statistics.Mode(arr);
+            Console.WriteLine($"mode of array: {mode}");
+            var mediana = Statistics.Mediana(arr);
+            Console.WriteLine($"mediana of array: {mediana}");
+            var mean= Statistics.Mean(arr);
+            Console.WriteLine($"mean of array: {mean}");
+            var maximum = Statistics.Maximum(arr);
+            Console.WriteLine($"maximum of array: {maximum}");
+            var minimum = Statistics.Minimum(arr);
+            Console.WriteLine($"minimum of array: {minimum}");
+            var span = Statistics.Span(arr);
+            Console.WriteLine($"span of array: {span}");
         }
         
     }
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -28,5 +28,64 @@ namespace Statistics
             }
             return mode;
         }
+
+        public static double Mediana(double[] arr)
+        {
+            double mediana;
+
+            if (arr.Length % 2 == 0)
+            {
+                mediana = (arr[arr.Length / 2 - 1] + arr[arr.Length / 2]) / 2;
+            }
+            else
+            {
+                mediana = arr[arr.Length / 2];
+            }
+            return mediana;
+        }
+        
+        public static double Mean(double[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum / arr.Length;
+        }
+
+        public static double Maximum(double[] arr)
+        {
+            double maximum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (maximum < arr[i])
+                {
+                    maximum = arr[i];
+                }
+            }
+
+            return maximum;
+        }
+
+        public static double Minimum(double[] arr)
+        {
+            double minimum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (minimum > arr[i])
+                {
+                    minimum= arr[i];
+                }
+            }
+
+            return minimum;
+        }
+
+        public static double Span(double[] arr)
+        {
+            return Maximum(arr) - Minimum(arr);
+        }
     }
 }
